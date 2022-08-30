2132aec6d492e149b1a75f1f3a1a66fa72160bde
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-08-30 11:22:46 -0700

    Remove scripts folder

--- SILVA_138.1/scripts/00-install-latest-qiime.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -i
-conda update conda
-wget https://data.qiime2.org/distro/core/qiime2-2022.8-py38-linux-conda.yml
-mamba env create -n qiime2-2022.8 --file qiime2-2022.8-py38-linux-conda.yml
-# OPTIONAL CLEANUP
-rm qiime2-2022.8-py38-linux-conda.yml
