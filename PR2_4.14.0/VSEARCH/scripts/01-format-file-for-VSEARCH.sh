3cf4d4b29c55314eadc3f662db9cb42b8669993c
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-08-30 16:43:36 -0700

    Update repo

--- PR2_4.14.0/VSEARCH/scripts/01-format-file-for-VSEARCH.sh
+++ PR2_4.14.0/VSEARCH/scripts/01-format-file-for-VSEARCH.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -i
-conda activate bbmap-env
+conda activate seqtk-env
+#conda activate bbmap-env
 
 #download UTAX version, which has correct headers
 wget https://github.com/pr2database/pr2database/releases/download/v4.14.0/pr2_version_4.14.0_SSU_UTAX.fasta.gz
@@ -8,5 +9,14 @@ gunzip pr2_version_4.14.0_SSU_UTAX.fasta.gz
 #get IDs after qiime curation, removing the caret character
 grep ">" exported-fasta/dna-sequences.fasta | sed 's/>//' > RESCRIPt-curated.ids.txt 
 
+#get full strings from fasta file using grep (slow!)
+while read line ; do grep $line pr2_version_4.14.0_SSU_UTAX.fasta ; done < RESCRIPt-curated.ids.txt > matches.txt
+
+#clean up, remove duplicates
+sed 's/>//' matches.txt | sort | uniq > matches.uniq.txt
+
+#pull out all fasta sequences using seqtk from UTAX file
+seqtk subseq pr2_version_4.14.0_SSU_UTAX.fasta matches.uniq.txt > 220830_PR2-4.14.0_VSEARCH-formatted.fasta
+
 #thank goodness for Brian Bushnell's BBTools which can take a substring match
-filterbyname.sh in=pr2_version_4.14.0_SSU_UTAX.fasta substring=t out=220830_PR2-4.14.0_VSEARCH-formatted.fasta names=RESCRIPt-curated.ids.txt
+#filterbyname.sh in=pr2_version_4.14.0_SSU_UTAX.fasta substring=t out=220830_PR2-4.14.0_VSEARCH-formatted.fasta names=RESCRIPt-curated.ids.txt
