de9fa02d68f23e8ef3a4b01c0ec70358fefd2d9b
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-08-30 12:36:04 -0700

    Update repo

--- PR2_4.14.0/qiime2/scripts/06-deduplicate-train-classifier.sh
+++ PR2_4.14.0/qiime2/scripts/06-deduplicate-train-classifier.sh
@@ -2,14 +2,14 @@
 conda activate qiime2-2022.8
 
 qiime rescript dereplicate \
-    --i-sequences silva-138.1-ssu-nr99-seqs-515FY-926R.qza \
-    --i-taxa silva-138.1-ssu-nr99-tax-derep-uniq.qza \
+    --i-sequences pr2_version_4.14.0_SSU-seqs-515FY-926R.qza \
+    --i-taxa pr2_version_4.14.0_SSU-tax-derep-uniq.qza \
     --p-rank-handles 'silva' \
     --p-mode 'uniq' \
-    --o-dereplicated-sequences silva-138.1-ssu-nr99-seqs-515FY-926R-uniq.qza \
-    --o-dereplicated-taxa  silva-138.1-ssu-nr99-tax-515FY-926R-derep-uniq.qza
+    --o-dereplicated-sequences pr2_version_4.14.0_SSU-seqs-515FY-926R-uniq.qza \
+    --o-dereplicated-taxa  pr2_version_4.14.0_SSU-tax-515FY-926R-derep-uniq.qza
 
 qiime feature-classifier fit-classifier-naive-bayes \
-    --i-reference-reads silva-138.1-ssu-nr99-seqs-515FY-926R-uniq.qza \
-    --i-reference-taxonomy silva-138.1-ssu-nr99-tax-515FY-926R-derep-uniq.qza \
-    --o-classifier silva-138.1-ssu-nr99-515FY-926R-classifier.qza
+    --i-reference-reads pr2_version_4.14.0_SSU-seqs-515FY-926R-uniq.qza \
+    --i-reference-taxonomy pr2_version_4.14.0_SSU-tax-515FY-926R-derep-uniq.qza \
+    --o-classifier pr2_version_4.14.0_SSU-515FY-926R-classifier.qza
