ca18ce3a5cb170546822b067aeaf359dc17550da
jcmcnch <5956604+jcmcnch@users.noreply.github.com>
2022-08-30 17:24:36 -0700

    Update repo

--- PR2_4.14.0/VSEARCH/scripts/00-export-from-RESCRIPt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -i
-conda activate qiime2-2022.8
-qiime tools export --input-path ../qiime2/pr2_version_4.14.0_SSU-seqs-derep-uniq.qza  --output-path exported-fasta
--- PR2_4.14.0/VSEARCH/scripts/README.md
+++
@@ -1,1 +0,0 @@
-A set of scripts that might be useful for others!
